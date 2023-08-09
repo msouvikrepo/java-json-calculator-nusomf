4bc65845df8a51ea649f37760efd71305b4e12cb
CodeSubmit Team <CodeSubmit Team hello@info.codesubmit.io>
2023-08-09 15:24:37 +0000

    We wish you all the best! The CodeSubmit Team.

---
+++ src/main/java/de/itdesign/application/JsonCalculator.java
@@ -0,0 +1,22 @@
+package de.itdesign.application;
+
+public class JsonCalculator {
+
+    public static void main(String[] args) {
+        // Don't change this part
+        if (args.length == 3) {
+            // Path to the data file, e.g. data/data.xml
+            final String DATA_FILE = args[0];
+            // Path to the data file, e.g. operations/operations.xml
+            final String OPERATIONS_FILE = args[1];
+            // Path to the output file
+            final String OUTPUT_FILE = args[2];
+
+            // <your code here>
+
+        } else {
+            System.exit(1);
+        }
+    }
+}
+
